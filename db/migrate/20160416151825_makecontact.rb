9222c25a7b23e4c7ef18efc55163329ee47e4898
Daksh M <dakshm@thegamersverge.net>
2016-04-16 15:26:25 +0000

    contact, migration

---
+++ db/migrate/20160416151825_makecontact.rb
@@ -0,0 +1,11 @@
+class Makecontact < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :comments
+      
+      t.timestamp
+    end
+  end
+end
